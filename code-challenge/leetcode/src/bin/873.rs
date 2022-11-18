08438dbcebcddb5f50838860454392c4222f0acf
Neil.wrz <wangrunze13@huawei.com>
2022-11-18 01:24:07 -0800

    update
    Signed-off-by: Neil.wrz <wangrunze13@huawei.com>

---
+++ code-challenge/leetcode/src/bin/873.rs
@@ -0,0 +1,9 @@
+
+fn main() {
+    println!("find the fibonacci subsequence with longest length!");
+}
+
+
+fn len_longest_fib_subseq(arr: Vec<i32>) -> i32 {
+    
+}
--- code-challenge/leetcode/src/bin/891.rs
+++ code-challenge/leetcode/src/bin/891.rs
@@ -4,6 +4,30 @@ fn main() {
 }
 
 fn sum_subseq_widths(nums: Vec<i32>) -> i32 {
+    if nums.len() < 2 {
+        return 0
+    }
 
+    let mut nums = nums.clone();
+    let mut ret = 0;
+
+    nums.sort();
+
+    for i in 0..nums.len()-1 {
+        for j in i+1..nums.len() {
+            ret += (nums[j] - nums[i])*(1<<(j-i-1))
+        }
+    }
+
+    return ret 
+}
+
+fn dummy_sum(nums: Vec<i32>) -> i32 {
     return 0
+}
+
+#[test]
+fn test_sum_subseq_widths() {
+    let nums = vec![2, 2, 1];
+    assert_eq!(sum_subseq_widths(nums), 3);
 }
