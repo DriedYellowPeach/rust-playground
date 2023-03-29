c653ec7fa5d3ffcc4ac3993e6a86683c12d94472
neil <958862317@qq.com>
2023-03-29 17:07:27 +0800

    update trie

--- code-challenge/leetcode/src/data_structures/trie.rs
+++ code-challenge/leetcode/src/data_structures/trie.rs
@@ -1,10 +1,9 @@
-
 use std::collections::HashMap;
 
 struct Node {
     value: char,
-    children: HashMap<char, * mut Node>,
-    fail: * const Node,
+    children: HashMap<char, *mut Node>,
+    fail: *const Node,
 }
 
 impl Node {
@@ -17,8 +16,41 @@ impl Node {
     }
 }
 
-
 struct Trie {
-    root: * mut Node,
+    root: *mut Node,
 }
 
+impl Trie {
+    fn new() -> Self {
+        // leak the box here
+        let root_ptr = Box::into_raw(Box::new(Node::new('^')));
+        Trie { root: root_ptr }
+    }
+
+    fn insert(&mut self, word: String) {
+        let mut cur = self.root;
+        for c in word.chars() {
+            let cur_node = unsafe { &mut *cur };
+            // go to next child if exist
+            // if not, leak the box and insert first
+            let next_node = *cur_node.children.entry(c).or_insert_with(|| Box::into_raw(Box::new(Node::new(c))));
+            cur = next_node;
+        }
+    }
+
+    // print the trie
+    fn display(&self) {
+
+    }
+}
+
+#[cfg(test)]
+mod trie_tests {
+    use super::*;
+
+    #[test]
+    fn test_insert() {
+        let mut trie = Trie::new();
+        trie.insert("hello".to_string());
+    }
+}
