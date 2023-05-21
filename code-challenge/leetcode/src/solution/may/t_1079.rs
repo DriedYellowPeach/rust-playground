40cbba711947ca1754ec0116f4f2064428f56906
neil <958862317@qq.com>
2023-05-21 14:54:14 +0800

    update

--- code-challenge/leetcode/src/solution/may/t_1079.rs
+++ code-challenge/leetcode/src/solution/may/t_1079.rs
@@ -1,6 +1,34 @@
+use std::collections::HashMap;
+
 #[allow(dead_code)]
 fn num_tile_possibilities(tiles: String) -> i32 {
-    0
+    // counts is a character counts
+    // example: {A: 3, B: 2, C:1}
+    // let mut counts = HashMap::<char, usize>::new();
+    let mut counts = [0usize; 26];
+    for c in tiles.chars() {
+        // let v = counts.entry(c).or_insert(0);
+        // *v += 1;
+        counts[c as usize - 'A' as usize] += 1;
+    }
+    dfs(&mut counts) as i32 - 1
+}
+
+fn dfs(counts: &mut [usize]) -> usize {
+    let mut sum = 1;
+    if counts.iter().sum::<usize>() == 0 {
+        return 1;
+    }
+
+    for i in 0..26 {
+        if counts[i] > 0 {
+            counts[i] -= 1;
+            sum += dfs(counts);
+            counts[i] += 1;
+        }
+    }
+
+    sum
 }
 
 #[test]
---
+++ lab/src/bin/reborrow.rs
@@ -0,0 +1,25 @@
+
+
+fn main() {
+    let mut s = "hello".to_string();
+    let a = &mut s;
+    let b: &mut String = a;
+    // a.push_str("world");
+    // b.push_str("!");
+}
+
+
+fn last_or_push<'a>(vec: &'a mut Vec<String>) -> &'a String {
+
+    if let Some(s) = vec.last() { // borrows vec
+        // returning s here forces vec to be borrowed
+        // for the rest of the function, even though it
+        // shouldn't have to be
+        return s; 
+    }
+    
+    // Because vec is borrowed, this call to vec.push gives
+    // an error!
+    vec.push("".to_string()); // ERROR
+    vec.last().unwrap()
+}
